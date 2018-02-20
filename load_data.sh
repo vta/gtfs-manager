4f8aae0ddb9f1245a3478195a158db048d3a8542
linuxwebexpert <Linux.Web.Expert@Gmail.com>
2018-02-20 09:57:02 -0800

    Updating for Dockerfile

--- load_data.sh
+++ load_data.sh
@@ -9,7 +9,7 @@
 #
 ###
 MAILTO=""
-OTP_DIR="/srv/vta.amigocloud.com"
+OTP_DIR="/srv/tripplaner"
 DATA_DIR="$OTP_DIR/data"
 GTFSM="python $OTP_DIR/gtfs-manager/src/gtfs-manager/gtfsmanager.py"
 GTFSM_TEMP_DIR="$OTP_DIR/data_tmp"
@@ -45,7 +45,7 @@ cd $OTP_DIR/OTP
 pwd
 
 # Java startup configuration for OpenTripPlanner instance
-jrun="java -Xmx3G -Xverify:none -jar $OTP_DIR/OTP/target/otp-1.0.0-shaded.jar --build $OTP_DIR/data --cache $OTP_DIR/ned/ --verbose"
+jrun="java -Xmx6G -Xverify:none -jar $OTP_DIR/OTP/target/otp-1.2.0-shaded.jar --build $OTP_DIR/data --cache $OTP_DIR/ned/ --verbose"
 
 # Captures today's date for log file
 now=`date +%F`
@@ -66,6 +66,6 @@ else
 fi
 
 # Restart the supervisor service to reload the new Graph.obj
-sudo /usr/bin/supervisorctl restart vta:vta_otp
+#/usr/bin/supervisorctl restart vta:vta_otp
 
 exit 0
