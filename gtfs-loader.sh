cd35a83a022c892758318f7080adb41ad2cc20b1
linuxwebexpert <Linux.Web.Expert@Gmail.com>
2018-02-20 09:43:42 -0800

    Updated gtfs loader script for Dockerfile

--- gtfs-loader.sh
+++ gtfs-loader.sh
@@ -2,3 +2,4 @@
 MAILTO=""
 TIME="\t%E real,\t%U user,\t%S sys,\t%k signals"
 /usr/bin/time -a -o /srv/tripplanner/logs/time-`date +%F`.log /bin/bash -x /srv/tripplanner/gtfs-manager/load_data.sh >> /srv/tripplanner/logs/load_data-`date +%F`.log
+
