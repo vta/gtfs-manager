cc081410f4f025d8867ba6476a3633bdf0024b6b
bijanmmarkes <bijan@stagerap.com>
2018-03-15 12:32:57 -0700

    uncomment OSM retrieval.

--- load_data.sh
+++ load_data.sh
@@ -19,19 +19,19 @@ cd $DATA_DIR
 pwd
 
 # Safely use curl with options to download OpenStreetMap latest map archive with error handling
-#curl -f -L -w "HTTP Result Code: %{http_code}\nContent-Type: %{content_type}\nFilesize: %{size_download}\nTotal Time: %{time_total}" -o map.osm.bz2 https://s3-us-west-2.amazonaws.com/gis-busstops-inventory/OSM+bayarea+network+older+prod+version/map.osm.bz2
-#res=$?
-#if test "$res" != "0"; then
-#    printf "ERROR Curl fetch of OpenStreetMap failed - exit code: $res"
-#elif test "$res" == "0"; then
-#    rm -f map.osm
-#    bunzip2 map.osm.bz2
-#    res=$?
-#    if test "$res" != "0"; then
-#        printf "ERROR OSM archive is corrupted - exit code: $res"
-#     	exit $res
-#    fi
-#fi
+curl -f -L -w "HTTP Result Code: %{http_code}\nContent-Type: %{content_type}\nFilesize: %{size_download}\nTotal Time: %{time_total}" -o map.osm.bz2 https://s3-us-west-2.amazonaws.com/gis-busstops-inventory/OSM+bayarea+network+older+prod+version/map.osm.bz2
+res=$?
+if test "$res" != "0"; then
+    printf "ERROR Curl fetch of OpenStreetMap failed - exit code: $res"
+elif test "$res" == "0"; then
+    rm -f map.osm
+    bunzip2 map.osm.bz2
+    res=$?
+    if test "$res" != "0"; then
+        printf "ERROR OSM archive is corrupted - exit code: $res"
+     	exit $res
+    fi
+fi
 
 # Change to data tmp directory and confirm
 cd $GTFSM_TEMP_DIR
